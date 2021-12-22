79348ba520820a5fdcd3f235fc675f70b5e69944
mateuszGorczany <mateusz.g64@interia.pl>
2021-12-22 21:03:27 +0100

    fix db conn

--- dashboard/database.py
+++ dashboard/database.py
@@ -2,6 +2,12 @@ from typing import Callable, Any
 from neo4j import GraphDatabase, basic_auth
 from neo4j.work.simple import Session
 
+def with_session(function: Callable):
+    def do_in_session(self, *args, **kwargs):
+        with self.driver.session() as session:
+            return function(self, session, *args, **kwargs)
+    return do_in_session
+
 class Database:
 
     def __init__(self, uri: str, user: str, password: str) -> None:
@@ -10,17 +16,11 @@ class Database:
     def close(self):
         self.driver.close()
 
-    @staticmethod
-    def _with_session(function: Callable):
-        def do_in_session(self, *args, **kwargs):
-            with self.driver.session() as session:
-                return function(self, session, *args, **kwargs)
-        return do_in_session
 
-    @_with_session
+    @with_session
     def write(self, session: Session, function: Callable, *args, **kwargs) -> Any:
         return session.write_transaction(function, *args, **kwargs)
 
-    @_with_session
+    @with_session
     def read(self, session: Session, function: Callable) -> Any:
         return session.read_transaction(function)
